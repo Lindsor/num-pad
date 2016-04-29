d87db81e309f5e01210420bdbb6242ff5cad5e84
Chris Shepherd <chris@chrisshepherd.me>
2016-04-29 10:07:09 +0100

    Adds ability to handle negative numbers

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = exports = function (num) {
-  return (num < 10) : "0" + num : "" + num;
+  if (num >= 0) {
+    return (num < 10 ? '0' : '') + num;
+  } else {
+    return (num > -10 ? '-0' : '-') + Math.abs(num);
+  }
 }
